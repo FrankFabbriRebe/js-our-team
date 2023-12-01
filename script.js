677a4006e01dd0d8f24771d51e288773571bf690
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-12-01 15:26:20 +0100

    add: arrayTeamMembers

---
+++ script.js
@@ -0,0 +1,40 @@
+// create array 
+const arrayTeamMembers = [
+
+    {
+        'nome' : 'Wayne Barnett',
+        'ruolo' : 'Founder & CEO',
+        'foto' : 'asset/img/wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        'nome' : 'Angela Caroll',
+        'ruolo' : 'Chief Editor',
+        'foto' : 'asset/img/angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        'nome' : 'Walter Gordon',
+        'ruolo' : 'Office Manager',
+        'foto' : 'asset/img/walter-gordon-office-manager.jpg',
+    },
+
+    {
+        'nome' : 'Angela Lopez',
+        'ruolo' : 'Social Media Manager',
+        'foto' : 'asset/img/angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        'nome' : 'Scott Estrada',
+        'ruolo' : 'Developer',
+        'foto' : 'asset/img/scott-estrada-developer.jpg',
+    },
+
+    {
+        'nome' : 'Barbara Ramos',
+        'ruolo' : 'Graphic Designer',
+        'foto' : 'asset/img/barbara-ramos-graphic-designer.jpg',
+    },
+
+];
